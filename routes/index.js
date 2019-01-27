300b3ce02f5a343f9c69ef1a8817c0d9fa91edf8
Ertuğrul BEKTİK <E.Bektik@gmail.com>
2019-01-27 22:47:20 +0300

    Chat UI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  if (!req.user) {
+    res.render('index', { title: 'Express' });
+  } else {
+    res.redirect('/chat');
+  }
 });
 
 module.exports = router;
